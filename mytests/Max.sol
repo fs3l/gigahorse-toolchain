78c4acdc82aceacbf8936a5910b2dba76cb97ea1
Urmi Biswas <urmi@Urmis-MacBook-Air.local>
2026-09-19 13:20:35 -0400

    update contract
    Signed-off-by: Urmi Biswas <urmi@Urmis-MacBook-Air.local>

--- mytests/Max.sol
+++ mytests/Max.sol
@@ -1,14 +1,11 @@
-// SPDX-License-Identifier: MIT
-  pragma solidity ^0.8.0;
-
-  contract Max {
-      function max(uint256 b) public pure returns (uint256) {
-          if (b > 1) {
-              b = b + 1;
-          } else {
-              b = b - 1;
-          }
-          return b;
-      }
-  }
-
+contract Max {
+    uint256 b =10;
+    uint256 a =1;
+    uint256 c =1;
+    function max() public returns (uint256) {
+        if (c > 1) {
+            b = a +1;
+        } else {
+            b = b - 1;
+        }
+       return b;}}
