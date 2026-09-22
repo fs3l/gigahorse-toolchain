2b9aa5841f3ed1cd3bbd05845279eea1cbc6fa35
Urmi Biswas <urmi@Urmis-MacBook-Air.local>
2026-09-22 18:03:52 -0400

    comments

--- mytests/Max.sol
+++ mytests/Max.sol
@@ -1,11 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
 contract Max {
-    uint256 b =10;
-    uint256 a =1;
-    uint256 c =1;
-    function max() public returns (uint256) {
-        if (c > 1) {
-            b = a +1;
+    function max(uint256 a, uint256 b) public pure returns (uint256) {
+        if (a > b) {
+            return a;
         } else {
-            b = b - 1;
-        }
-       return b;}}
+            return b;
+        }}}
+
+
